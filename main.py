1fc8a799c04e70f9b67f2842c31f4b79d26741a1
HassanJaami <97073848+HassanJaami@users.noreply.github.com>
2023-09-20 10:41:36 -0700

    TitlesExtraction.py

--- main.py
+++ main.py
@@ -16,3 +16,33 @@ print(element.get_attribute('type'))
 # element.clear()
 element.send_keys("Python_Tutorial")
 element.send_keys(Keys.RETURN)
+
+try:
+
+
+    # Create a Chrome WebDriver with the service
+    driver = webdriver.Chrome()
+
+    # Navigate to the YouTube video URL
+    url = ("https://foodtank.com/news/category/sustainable-agriculture/")
+    driver.get(url)
+
+    elems=driver.find_elements(By.CSS_SELECTOR, """.single_category_title""")
+    print(len(elems))
+    for i in elems:
+        print(i.text)
+
+    # ... do other actions on the page if needed ...
+
+    # Close the WebDriver when done
+    driver.quit()
+
+except WebDriverException as e:
+    print(f"An error occurred: {e}")
+
+
+
+
+
+
+
