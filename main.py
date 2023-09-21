6093b537c602707714a052cc03c2ee859cdac996
Hassan Ali <mhassan.ali7879@gmail.com>
2023-09-20 19:12:31 -0700

    1

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 
-# Sending value to search
-driver=webdriver.Chrome()
-driver.get("https://www.python.org")
 
-element=driver.find_element(By.XPATH, """//*[@id="id-search-field"]""")
-print(element.get_attribute('type'))
-# element.clear()
-element.send_keys("Python_Tutorial")
-element.send_keys(Keys.RETURN)
+
+
+
+
+
+
+
+
 
 try:
 
